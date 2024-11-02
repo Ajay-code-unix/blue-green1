f72d62376a96a57f6a04666aa6f31a31c52db797
Ajay <badhrinr@gmail.com>
2024-11-03 00:15:01 +0530

    switch 1

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express=require('express')
 const app = express();
 const PORT=3000;
-app.get('/',(req,res)=> res.send('Hello There!!!'));
+app.get('/',(req,res)=> res.send('Hello 1 There!!!'));
 app.listen(PORT,()=>console.log(`APP running on Port ${PORT}`));
 
